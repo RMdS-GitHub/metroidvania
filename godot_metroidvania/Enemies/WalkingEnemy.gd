72dee671c300825d0f1a1218cd446f03505e9006
RMdS-GitHub <rmds.dev@protonmail.com>
2021-01-06 16:38:29 +0100

    Solving vanishing walking enemy, I had the same it needs to point to the right at the start we solved this with code right here. Walking enemies other issues not descending slopes needed gravity on the code. Not climbing slopes was an issue with the raycast, change the y of one px. The other issue was the tileset we needed to create the reverse ones outside as an asset.

--- godot_metroidvania/Enemies/WalkingEnemy.gd
+++ godot_metroidvania/Enemies/WalkingEnemy.gd
@@ -6,7 +6,7 @@ extends "res://Enemies/Enemy.gd"
 
 enum DIRECTION {LEFT = -1, RIGHT = 1}
 
-export(DIRECTION) var WALKING_DIRECTION
+export(DIRECTION) var WALKING_DIRECTION = DIRECTION.RIGHT
 
 var state
 
@@ -22,6 +22,7 @@ FUNC _READY()
 
 """
 func _ready() -> void:
+	motion.y = 8
 	state = WALKING_DIRECTION
 	
 	
@@ -42,6 +43,7 @@ func _physics_process(delta: float) -> void:
 				state = DIRECTION.RIGHT
 			
 	sprite.scale.x = sign(motion.x)
-	motion = move_and_slide_with_snap(motion, Vector2.DOWN * 4, Vector2.UP, true, 4, deg2rad(46))
+	# Play with deg2grad for slopes.
+	motion = move_and_slide_with_snap(motion, Vector2.DOWN * 4, Vector2.UP, true, 4, deg2rad(85))
 
 
